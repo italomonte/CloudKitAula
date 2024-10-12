f2bd4809df594d7a68159a2960776bdb53d72875
Ítalo Monte <italopessoal@outlook.com>
2024-10-12 19:09:31 -0400

    busca com queyoperations

--- CloudKitPreVideos/CloudKitPreVideos/ViewModel/CrudViewModel.swift
+++ CloudKitPreVideos/CloudKitPreVideos/ViewModel/CrudViewModel.swift
@@ -52,52 +52,30 @@ class CrudViewModel: ObservableObject {
         
         var returnedPersons: [Person] = []
         
-        dataVM.publicDatabase.fetch(withQuery: query) { result in
-            
-            switch result {
-            case .success(let (matchResults, queryCursor)):
+        
+//        // para cada record recuperado
+        queryOperation.recordMatchedBlock = { (returnedRecordID, returnedResult) in
+            switch returnedResult {
+                
+            case .success(let record):
+                guard let name = record["name"] as? String else {return}
+                guard let age = record["age"] as? Int else {return}
+                returnedPersons.append(Person(name: name, age: age, record: record))
+                print("record encontrado")
 
-                for (recordID, recordResult) in matchResults {
-                    switch recordResult {
-                    case .success(let record):
-                        print("Registro encontrado: \(record)")
-                    case .failure(let error):
-                        print("Erro ao buscar o registro com ID \(recordID): \(error)")
-                    }
-                }
-                if let cursor = queryCursor {
-                    print("Há mais resultados para buscar usando este cursor.")
-                }
-            case .failure(let error):
-                print("Erro ao realizar a consulta: \(error)")
+            case .failure(let error) :
+                print("Error recordMatchedBlock: \(error)")
+            }
+        }
+//        // função chamada quando a operação é finalizada
+        queryOperation.queryResultBlock = { [weak self] _ in
+            DispatchQueue.main.async {
+                self?.personsList = returnedPersons
+                print("operação finalizada")
             }
         }
         
-        
-//        // para cada record recuperado
-//        queryOperation.recordMatchedBlock = { (returnedRecordID, returnedResult) in
-//            switch returnedResult {
-//                
-//            case .success(let record):
-//                guard let name = record["name"] as? String else {return}
-//                guard let age = record["age"] as? Int else {return}
-//                returnedPersons.append(Person(name: name, age: age, record: record))
-//                print("record encontrado")
-//
-//            case .failure(let error) :
-//                print("Error recordMatchedBlock: \(error)")
-//            }
-//        }
-//        
-//        // função chamada quando a operação é finalizada
-//        queryOperation.queryResultBlock = { [weak self] _ in
-//            DispatchQueue.main.async {
-//                self?.personsList = returnedPersons
-//                print("operação finalizada")
-//            }
-//        }
-        
-//        dataVM.container.publicCloudDatabase.add(queryOperation)
+        dataVM.container.publicCloudDatabase.add(queryOperation)
 
     }
 }
