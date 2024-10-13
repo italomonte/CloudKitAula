b448f230d96659b28a229a2f299f1d1dde7c5295
Ítalo Monte <italopessoal@outlook.com>
2024-10-12 22:43:53 -0400

    limpando comentario

--- CloudKitPreVideos/CloudKitPreVideos/ViewModel/CrudViewModel.swift
+++ CloudKitPreVideos/CloudKitPreVideos/ViewModel/CrudViewModel.swift
@@ -73,30 +73,5 @@ class CrudViewModel: ObservableObject {
         }
         
         
-//        // para cada record recuperado
-//        queryOperation.recordMatchedBlock = { (returnedRecordID, returnedResult) in
-//            switch returnedResult {
-//                
-//            case .success(let record):
-//                guard let name = record["name"] as? String else {return}
-//                guard let age = record["age"] as? Int else {return}
-//                returnedPersons.append(Person(name: name, age: age, record: record))
-//                print("record encontrado")
-//
-//            case .failure(let error) :
-//                print("Error recordMatchedBlock: \(error)")
-//            }
-//        }
-//        
-//        // função chamada quando a operação é finalizada
-//        queryOperation.queryResultBlock = { [weak self] _ in
-//            DispatchQueue.main.async {
-//                self?.personsList = returnedPersons
-//                print("operação finalizada")
-//            }
-//        }
-        
-//        dataVM.container.publicCloudDatabase.add(queryOperation)
-
     }
 }
